606ceea07c3d25b90fc6f1f4c5b0aeefb428c2a0
shelpyakovstepan <step3210shelpyakov@gmail.com>
2025-07-10 18:30:11 +0300

    Добавил ветку develop, добавил проверочный метод ping

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, FastAPI
+
+app = FastAPI()
+
+router = APIRouter(
+    prefix="/test",
+    tags=["api"],
+)
+
+#app.include_router(router)
+
+@app.get("//")
+async def ping():
+    return {"data": "pong"}
