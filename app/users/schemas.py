09a52a511fc4ca61f63a6d97d1e585f0af24e6e6
shelpyakovstepan <step3210shelpyakov@gmail.com>
2025-07-12 18:03:48 +0300

    Исправил баг, из-за которого можно было добавить что хочешь в поле номера при регистрации

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,18 +1,28 @@
 # THIRDPARTY
-from pydantic import BaseModel, EmailStr, Field
+import re
+
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
 class SUsersRegister(BaseModel):
     email: EmailStr
-    phone_number: str = Field(min_length=11, max_length=12)
+    phone_number: str
     surname: str = Field(max_length=20)
     name: str = Field(max_length=20)
     password: str = Field(min_length=5, max_length=15)
 
+    @field_validator("phone_number")
+    @classmethod
+    def validate_phone_number(cls, values: str) -> str:
+        if not re.match(r'^\+\d{11}$', values):
+            raise ValueError('Номер телефона должен начинаться с "+" и содержать 11 цифр')
+        return values
+
 
 class SUsersLogin(BaseModel):
     email: EmailStr
-    password: str = Field(min_length=5, max_length=15)
+    password: str
+
 
 
 # class SUsers(BaseModel):
